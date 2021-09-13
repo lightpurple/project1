a508a0aa0509cd03f33c8590bd702d464b2365b9
lightpurple <dmstjd10@gmail.com>
2021-09-13 14:44:10 +0900

    freeze

--- backend/config/board/models.py
+++ backend/config/board/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='post')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_at = models.DateField(auto_now_add=True)
@@ -12,7 +12,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name='comment')
     text = models.CharField(max_length=200)
     created_at = models.DateField(auto_now_add=True)
